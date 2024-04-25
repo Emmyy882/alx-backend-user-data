dbaa01524a378c9b6fd151ca01029a4668e08050
Emmyy882 <emmaagbaje3@gmail.com>
2024-04-25 21:19:54 +0000

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,7 +3,7 @@
 import bcrypt
 
 
-def _hash_password(password: str) -> str:
+def _hash_password(password: str) -> bytes:
     """returns bytes of a string argument"""
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
